4b894ede1d43e1593c75a8ea90ebf1d2a7b6033a
anilu556 <anilu556@hotmail.com>
2019-01-31 15:04:51 -0600

    sprint 1, components

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+
+  }
+
+  render(){
+  return()
+    <h1>
+    This is home
+    </h1>
+  }
+}
+
+export deafult Home
---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class LogIn extends Component{
+  constructor(){
+  super();
+  }
+
+  render(){
+  return()
+    <div>
+    </div>
+  }
+}
+
+export deafult LogIn
